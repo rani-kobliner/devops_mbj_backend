fa8841f3702d3de1fde8c6a3e353949cae0f8c17
קובלינר רני בתיה <  @mbjcomp.local>
2024-12-13 10:16:06 +0200

    change

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get('/api', (req, res) => {
     res.send({ message: 'Hello from backend!' });
 });
 
+app.get('/', (req, res) => { // For health check
+    res.status(200).json({ status: 'UP' });
+  });
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
